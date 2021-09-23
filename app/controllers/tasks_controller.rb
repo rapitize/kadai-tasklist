29f9f4b0b99862d5ff3376872fc125d170db9b60
zukABi <dlog8meno_4798msm@yahoo.co.jp>
2021-09-23 15:11:37 +0000

    fix2nd

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,14 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  #before_action :correct_user, only: [:update, :destroy]
 
 
   def index
     @tasks = Task.all
+    if logged_in?
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  get 'users/new'
   root to: 'tasks#index'
 
   get 'login', to: 'sessions#new'
