ddbe2f63b7027b5d5a0c6003c395b788ea635561
zukABi <dlog8meno_4798msm@yahoo.co.jp>
2021-09-23 11:55:48 +0000

    for heroku

--- db/migrate/20210923052921_add_user_to_tasks.rb
+++ db/migrate/20210923052921_add_user_to_tasks.rb
@@ -1,5 +1,10 @@
 class AddUserToTasks < ActiveRecord::Migration[6.1]
   def change
-    add_reference :tasks, :user, null: false, foreign_key: true
+    #add_column :references, :user, null: false, foreign_key: true
+    add_column :tasks, :user, :references, null: false, foreign_key: true
+    
+    #create_table :tasks do |t|
+    #  t.references :user, null: false, foreign_key: true
+    #end
   end
 end
